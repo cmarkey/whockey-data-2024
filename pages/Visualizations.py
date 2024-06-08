ccd6aee252019e20a1bb851ba2532701256a2a97
cmarkey <carleenmarkey@gmail.com>
2024-06-07 20:07:47 -0400

    corrected name

--- pages/Visualizations.py
+++ pages/Visualizations.py
@@ -13,4 +13,4 @@ df = df_raw.loc[df_raw["GP"] >= df_raw["GP"].max()/2,['Goals', 'Assists', 'Point
 median = df.median(numeric_only=True)
 labels = pd.DataFrame(columns=df.columns)
 
-player_select = st.selectbox("Player", options = df_names.Name.unique())
+player_select = st.selectbox("Player", options = df_names.Player.unique())
